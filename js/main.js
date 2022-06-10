aa2bb65a9be4ff61cff4869ea661a759a40f4f98
Alexey Polyakov <alexweb@internet.ru>
2022-06-10 14:39:14 +0300

    Вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+getRandomIntInclusive(0, 20);
+
+function truncate(str) {
+  let maxlegth = 140;
+  if (str.length < maxlegth) {
+    return true;
+  } else {
+    return false;
+  }
+}
