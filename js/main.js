4278aeb8e44ef62451eefa110f09b4052c27cbc2
Alexey Polyakov <alexweb@internet.ru>
2022-06-12 10:52:14 +0300

    добавил в параметрах  str, добавил сообщение при false

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно ссылка на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
   if (min && max >= 0) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+    min = Math.ceil(min >= 0);
+    max = Math.floor(max >= 0);
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
   }
+  return 'Заданный диапазон не соответсвует условию!';
+
 }
 getRandomIntInclusive(0, 20);
 
 
 //Функция для проверки максимальной длины строки, ссылка на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
-function checkMaxLength(maxLegth) {
-  if ('str'.length < maxLegth) {
-    return 'str'.length < 'maxlegth';
+function checkMaxLength(str, maxLegth) {
+  const maxLength = 140;
+  if (str.length < maxLegth) {
+    return str.length < maxLength;
   }
 }
-checkMaxLength(140);
+checkMaxLength(100);
