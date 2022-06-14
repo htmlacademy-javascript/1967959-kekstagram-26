ce6befeb0cbf0b025deeef2753c7e61cd3266dd4
Alexey Polyakov <alexweb@internet.ru>
2022-06-14 21:16:56 +0300

    Обновил функции

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно ссылка на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIntInclusive(min, max) {
-  if (min >= 0 && max >= 0) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+/**
+ * Вернёт целое число в диапазоне `min`, `max`.
+ * @param {number} min Целое положительное число.
+ * @param {number} max Целое положительное число больше или равное `min`.
+ */
+const getIntegerInRange = (min, max) => {
+  const range = [min, max];
+
+  if (!range.every(Number.isInteger)) {
+    throw new RangeError(`Нецелочисленный диапазон: [${range}]`);
   }
+  if (min < 0 || max < 0 || min > max) {
+    throw new RangeError(`Неподдерживаемый диапазон: [${range}]`);
+  }
+  const value = (max - min) * Math.random() + min;
+  return Math.round(value);
+};
 
-  return 'Заданный диапазон не соответсвует условию!';
-}
-getRandomIntInclusive(0, 20);
+getIntegerInRange(1, 3);
 
+/**
+ * Проверит максимальную длину строки.
+ * @param {string} value Проверяемая строка.
+ * @param {number} maxLength Максимальная длинна строки.
+ */
+const validateMaxLength = (value, maxLength = 140) => {
+  if (typeof value !== 'string') {
+    throw new Error(`Не является строкой: ${value}`);
+  }
+  if (!Number.isInteger(maxLength) || maxLength < 0) {
+    throw new Error(`Неверный параметр maxLength: ${maxLength}`);
+  }
+  return value.length <= maxLength;
+};
 
-//Функция для проверки максимальной длины строки, ссылка на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
-function checkMaxLength(str, maxLength) {
-  return str.length < maxLength;
-}
-checkMaxLength(100,140);
+validateMaxLength('');
