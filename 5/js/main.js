11ec3cdb75ca0ca93e15ed251a2603f4d025ab59
Keks <keks@htmlacademy.ru>
2022-07-04 12:39:06 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,9 @@
+import createThumbnailsFragment from './post-thumbnail.js';
+import generatePosts from './post-generator.js';
+
+// Миниатюры публикаций.
+
+const posts = generatePosts().sort(() => Math.random() - .5);
+const thumbnailsContainerElement = document.querySelector('.pictures');
+
+thumbnailsContainerElement.append(createThumbnailsFragment(posts));
