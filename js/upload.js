11ea45ea2602f5fc4be879bb07f3b8051f575300
Alexey Polyakov <alexweb@internet.ru>
2022-07-19 20:47:36 +0300

    Довит масштабирование изображения

--- js/upload.js
+++ js/upload.js
@@ -1,5 +1,6 @@
 import openModal from './modal.js';
 import createConstrainer from './upload-constrainer.js';
+import initScaleControl from './upload-scale-control.js';
 
 /**
  * Форма публикации.
@@ -14,13 +15,34 @@ const formElement = document.querySelector('#upload-select-image');
 const modalElement = formElement.querySelector('.img-upload__overlay');
 
 /**
+ * Редактируемое изображение.
+ * @type {HTMLImageElement}
+ */
+const imageElement = modalElement.querySelector('.img-upload__preview img');
+
+/**
+ * Управление масштабом.
+ */
+const scaleControlElement = initScaleControl(
+  modalElement.querySelector('.img-upload__scale')
+);
+
+/**
  * Методы установки ограничений для Hashtags и описаний.
  */
-const constraints = createConstrainer(formElement, {
+const constrainer = createConstrainer(formElement, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'text__error'
 });
+
+/**
+ * Применит масштаб.
+ * @param {CustomEvent} event
+ */
+const handleScaleControlUpdate = (event) => {
+  imageElement.style.transform = `scale(${event.detail.percent / 100})`;
+};
 
 /**
  * Откроет окно редактирования.
@@ -30,12 +52,16 @@ const handleFileChange = () => {
   openModal(modalElement);
 };
 
-//openModal(modalElement);
+openModal(modalElement);
 
+// Реакция на выбор файла
 formElement.filename.addEventListener('change', handleFileChange);
 
+// Реакция на изменение масштаба
+scaleControlElement.addEventListener('update', handleScaleControlUpdate);
+
 // Ограничения для меток и описания.
-constraints
+constrainer
   .setHashtagsSyntax()
   .setHashtagsRepetitionConstraint()
   .setHashtagsMaxItemLength(20)
